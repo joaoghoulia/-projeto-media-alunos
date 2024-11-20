b464ce904d4afc0b529c07619ce96b095fc39135
joaoghoulia <m4530569@gmail.com>
2024-11-20 09:46:44 -0300

    media

---
+++ notas.py
@@ -0,0 +1,10 @@
+# Recebe as 3 notas do aluno
+nota1 = float(input("Digite a 1ª nota: "))
+nota2 = float(input("Digite a 2ª nota: "))
+nota3 = float(input("Digite a 3ª nota: "))
+
+# Calcula a média aritmética
+media = (nota1 + nota2 + nota3) / 3
+
+# Exibe a média
+print(f"A média do aluno é: {media:.2f}")
