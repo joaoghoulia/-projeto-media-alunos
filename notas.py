19bf218681faf9ea2d9fe7fa23287fb1ca8f7912
joaoghoulia <m4530569@gmail.com>
2024-11-20 11:16:51 -0300

    ponderado

--- notas.py
+++ notas.py
@@ -1,21 +1,13 @@
-# Recebe as 3 notas do aluno
-nota1 = float(input("Digite a 1ª nota: "))
-nota2 = float(input("Digite a 2ª nota: "))
-nota3 = float(input("Digite a 3ª nota: "))
 
-# Calcula a média aritmética
-media = (nota1 + nota2 + nota3) / 3
-
-# Exibe a média
-print(f"A média do aluno é: {media:.2f}")
-
-exibir_resultado(media)
-
-def exibir_resultado(media):
-    # Exibe o resultado com base na média
-    if media > 6:
-        print("Aprovado")
-    elif 5.0 < media <= 6.0:
-        print("Recuperação")
-    else:
-        print("Reprovado")
+ 
+# Cálculo da média ponderada
+media = (nota1 * 1 + nota2 * 1 + nota3 * 2) / 4
+ 
+print(f"Sua média é: {media:.2f}")
+if media >= 6:
+   print("Você foi aprovado")
+elif 3 < media < 6:
+    print("Você está de recuperação")
+else:
+   print("Você foi reprovado")
+   
